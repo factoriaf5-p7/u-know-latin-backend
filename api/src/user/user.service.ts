046852f236b06f2da4710cf1d2151143f1f1300e
Arielparicio28 <arielaparicio100@gmail.com>
2023-06-22 14:32:50 +0200

    creando codigo de estados

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -21,12 +21,13 @@ export class UserService {
     return this.userModel.findOne({ _id: id }).exec();
   }
   async update(id: string, updateUserDto: UpdateUserDto) {
-    Object.assign(updateUserDto);
+    const user = await this.userModel.findById(id);
+    if (!user) {
+      throw new HttpException('User not Found', HttpStatus.BAD_REQUEST);
+    }
+    Object.assign(user, updateUserDto);
 
-    const updatedUser = await this.userModel
-      .findByIdAndUpdate({ _id: id })
-      .exec();
-     console.log(updatedUser)
+    const updatedUser = await user.save();
     return updatedUser;
   }
 
@@ -34,32 +35,9 @@ export class UserService {
     const deletedUser = await this.userModel
       .findByIdAndRemove({ _id: id })
       .exec();
+    if (!deletedUser) {
+      throw new HttpException('User not usaga Found', HttpStatus.BAD_REQUEST);
+    }
     return deletedUser;
   }
 }
-
-/* @Injectable()
-export class UserService {
-  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.UserModel(createUserDto);
-    return createdUser.save();
-  }
-
-  async findAll() {
-    return this.UserModel.find();
-  }
-
-  async findOne(id: number) {
-    return this.UserModel.findOne({ _id: id }).exec();
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} hola mundo`;
-  }
-}
- */
