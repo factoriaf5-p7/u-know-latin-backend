dd49cc77e9fa7b09004fd57b968644a669be1305
Acrata <hxtr227@gmail.com>
2023-06-28 23:16:18 +0200

    revert auth test attemp

--- api/src/auth/controllers/auth.controller.spec.ts
+++ api/src/auth/controllers/auth.controller.spec.ts
@@ -1,45 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from '../services/auth.service';
-import { Request } from 'express'; 
+import { UserService } from '../../user/user.service';
+import { CreateUserDto } from '../../user/dto/create-user.dto';
+import { Request } from 'express';
+import { ParamsDictionary, ParsedQs } from 'express-serve-static-core';
 
 describe('AuthController', () => {
-  let controller: AuthController;
-  let service: AuthService;
-  const mockAuthService = {
-    signUp: jest
-      .fn()
-      .mockReturnValue(Promise.resolve({access_token: 'token here'}))
-  };
+  let authController: AuthController;
+  let authService: AuthService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [ AuthService ]
-    })
-      .overrideProvider(AuthService)
-      .useValue(mockAuthService)
-      .compile();
+      providers: [AuthService, UserService],
+    }).compile();
 
-    controller = module.get<AuthController>(AuthController);
-    service = module.get<AuthService>(AuthService);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('signup', () => {
+    it('should create a new user', async () => {
+      const createUserDto: CreateUserDto = {
+        // Provide the necessary properties for creating a user
+        // Replace these with the actual user data for testing
+        user_name: 'testuser',
+        password: 'testpassword',
+      };
+
+      const createdUser: CreateUserDto = {
+        id: '1',
+        name: 'John Doe',
+        user_name: 'johndoe',
+        password: 'password123',
+        email: 'johndoe@example.com',
+        wallet_balance: 100,
+        chat: 'Hello, World!',
+        id_published_content: [1, 2, 3],
+        id_bought_content: [4, 5],
+        created_at: new Date(),
+        created_update: new Date(),
+      };
+      
+
+      jest.spyOn(userService, 'create').mockResolvedValue(createdUser);
+
+      const result = await authController.signup(createUserDto);
+
+      expect(userService.create).toHaveBeenCalledWith(createUserDto);
+      expect(result).toBe(createdUser);
+    });
   });
 
-  it('should return an accessToken when a valid user is provided', async () => {
-    const req = {
-      body: {
+  describe('login', () => {
+    it('should generate a token for the authenticated user', async () => {
+      // Create a mock Request object
+      const req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>> = {
+        get: jest.fn(),
+        header: jest.fn(),
+        accepts: jest.fn(),
+        acceptsCharsets: jest.fn(),
+        // ... add other required properties and methods here
         user: {
-          email: 'test@mail.com',
-          password: '1234',
+          id: 1,
+          username: 'johndoe',
         },
-      },
-    } as Request;
-    expect(await controller.signup(req.body)).toMatchObject({
-      accessToken: 'token here',
+      };
+      const generatedToken = {
+        access_token: 'sdsdsdsd',
+      } // Replace with the expected generated token
+
+      jest.spyOn(authService, 'generateToken').mockReturnValue(generatedToken);
+
+      const result = await authController.login(req);
+
+      expect(authService.generateToken).toHaveBeenCalledWith(req.user);
+      expect(result).toBe(generatedToken);
     });
   });
 });
--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -10,5 +10,6 @@ import { User, UserSchema } from '../schemas/users.schema';
   ],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
