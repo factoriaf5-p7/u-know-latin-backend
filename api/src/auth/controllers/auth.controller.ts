bcaeb51520ffa40e81217f27d97e03c4e34f5cd3
Acrata <hxtr227@gmail.com>
2023-06-28 23:14:24 +0200

    Revert "Req response in login endpoint returns req.user from passport"
    This reverts commit b9ed981e7e584acbd52bc2ec3aadb556fc4f6056.

--- api/src/auth/controllers/auth.controller.ts
+++ api/src/auth/controllers/auth.controller.ts
@@ -17,7 +17,6 @@ export class AuthController {
   @UseGuards(AuthGuard('local'))
   @Post('login')
   login(@Req() req: Request) {
-    console.log(req.user)
-    return this.authService.generateToken(req.user);
+    return this.authService.generateToken(req.body);
   }
 }
