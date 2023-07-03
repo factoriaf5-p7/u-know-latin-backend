e3a463b48c09b63bf87147c0818024f9ec150cf4
Acrata <hxtr227@gmail.com>
2023-07-03 11:43:34 +0200

    fix merge typo

--- api/src/dto/create-comment.dto.ts
+++ api/src/dto/create-comment.dto.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { IsNotEmpty, IsString } from 'class-validator';
 import { User } from 'src/schemas/users.schema';
-=======
-import { IsNotEmpty, IsString} from "class-validator";
-import { User } from '../schemas/users.schema';
->>>>>>> origin/dev
 
 export class CreateCommentDto {
   @IsNotEmpty()
